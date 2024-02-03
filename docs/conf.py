f56668709689e783d2f82121eee45570c1c0606f
FusionPBX <markjcrane@gmail.com>
2024-02-03 15:56:52 -0700

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,24 @@
+# Configuration file for Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+
+project = 'FusionPBX'
+author = 'Multiple Authors'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+]
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
